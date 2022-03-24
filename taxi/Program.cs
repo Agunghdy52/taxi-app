53ab3ad85e2f6d138b22bac2af059a3e5a998101
Agunghy52 <agunghidayat2@sudents.amikom.ac.id>
2022-03-24 23:53:12 +0700

    taksi

---
+++ taxi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi_app
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Agung Hidayat";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
